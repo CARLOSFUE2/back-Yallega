79ebceb889a4ca1983389c9275a95b19450d26b9
CFUENTES2 <eduardofuentesingmeca@gmail.com>
2021-09-27 21:20:39 -0400

    wip

--- controllers/calculatePrice.js
+++ controllers/calculatePrice.js
@@ -7,17 +7,16 @@ const VALUESUNITARIES ={
 }
 
 async function price (distance, data) {
-    console.log(distance);
     let cost = 0;
     let deliverCost = 0;
     let rates = await Rate.find();
     rates = rates[0];
     if(distance <= 3){
         cost = rates.min;
-        deliverCost = cost * (minDistribution/100);
+        deliverCost = cost * (rates.minDistribution/100);
     }else{
         cost = (rates.min) + ( (distance - 3) * rates.forKm);
-        deliverCost = cost * (forKmdistribution/100);
+        deliverCost = cost * (rates.forKmdistribution/100);
     }
     return {cost, deliverCost}
 }
