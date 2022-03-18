fa3229b394964619d647af0b647d9cbd0d928574
CARLOSFUE2 <eduardofuentesingmeca@gmail.com>
2022-03-18 16:26:32 -0400

    acomodado lo del precio de los envios

--- controllers/calculatePrice.js
+++ controllers/calculatePrice.js
@@ -1,33 +1,32 @@
 const Rate = require("../models/rates")
 
-const VALUESUNITARIES ={
-    estandar: 200,
-    especial: 300,
-    vip:500
+const VALUESUNITARIES = {
+  estandar: 200,
+  especial: 300,
+  vip: 500,
 }
 
-async function price (distance, typeRateForClient) {
-    console.log(typeRateForClient)
-    let cost = 0;
-    let deliverCost = 0;
-    let rates = await Rate.find();
-    let rate;
-    
-    rates.forEach(rt =>{
-        if(rt.type == typeRateForClient){
-         rate = rt;   
-        } //ojo aqui
-    })
-    console.log(rate)
-    
-    if(distance <= 3){
-        cost = rate.min;
-        deliverCost = cost * (rate.minDistribution/100);
-    }else{
-        cost = (rate.min) + ( (distance - 3) * rate.forKm);
-        deliverCost = cost * (rate.forKmdistribution/100);
-    }
-    return {cost, deliverCost}
+async function price(distance, typeRateForClient) {
+  console.log(typeRateForClient)
+  let cost = 0
+  let deliverCost = 0
+  let rates = await Rate.find()
+  let rate
+
+  rates.forEach((rt) => {
+    if (rt.type == typeRateForClient) {
+      rate = rt
+    } //ojo aqui
+  })
+
+  if (distance <= 3) {
+    cost = rate.min
+    deliverCost = cost - cost * (rate.minDistribution / 100)
+  } else {
+    cost = rate.min + (distance - 3) * rate.forKm
+    deliverCost = cost - cost * (rate.forKmdistribution / 100)
+  }
+  return { cost, deliverCost }
 }
 
-module.exports = { price}
+module.exports = { price }
--- routes/request.js
+++ routes/request.js
@@ -1,235 +1,255 @@
-let express = require('express');
-let router = express.Router();
-const Request = require('../models/request');
-const User = require('../models/users');
-const Billing = require('../models/billing');
-const {requestMatrixDistane} = require('../controllers/distanceMatrix');
-const {price} = require('../controllers/calculatePrice');
-const Token = require('../models/tokens');
-const {sendNotification} = require('../controllers/notifications');
-const Client = require('../models/client');
-const STATUS =[
-    'En Espera', 'Asignado', 'En Proceso', 'Entregado', 'Rechazado', 'Devuelto', 'Retirado'];
+let express = require("express")
+let router = express.Router()
+const Request = require("../models/request")
+const User = require("../models/users")
+const Billing = require("../models/billing")
+const { requestMatrixDistane } = require("../controllers/distanceMatrix")
+const { price } = require("../controllers/calculatePrice")
+const Token = require("../models/tokens")
+const { sendNotification } = require("../controllers/notifications")
+const Client = require("../models/client")
+const STATUS = [
+  "En Espera",
+  "Asignado",
+  "En Proceso",
+  "Entregado",
+  "Rechazado",
+  "Devuelto",
+  "Retirado",
+]
 
-router.get('/', async (req,res) => {
-    const request = await Request.find();
-    res.json(request);
-});
-
-router.get('/client/:id', async (req,res) => {
-    const id = req.params.id;
-    let response =[]
-    const request = await Request.find();
-    const clients = await User.find();
-    let requestMatch = [];
-    request.forEach(e =>{
-       if(e.clientId == id){
-            requestMatch.push(e);
-       } ;
-    })
-    requestMatch.forEach(req =>{
-        let coincidencias = clients.filter( client => req.userId == client._id)
-        let formatResponse ={
-        _id:req._id,
-        product: req.product,
-        client:coincidencias[0].name,
-        numberPhone: coincidencias[0].numberPhone,
-        status: req.status,
-        price: req.value,
-        address: req.destiny
-        }
-        response.push(formatResponse)
-    })
-    res.json(response);
-});
-
-router.get('/deliver/:id', async (req,res)=>{
-    const id = req.params.id;
-    const requests = await Request.find({deliveryId: id});
-    res.send(requests);
+router.get("/", async (req, res) => {
+  const request = await Request.find()
+  res.json(request)
 })
 
-router.get('/:id', async (req, res) => {
-    const id = req.params.id;
-    const request = await Request.findById({_id:id});
-    const listClients = await User.find()
-    let clients = listClients.filter( client => request.userId == client._id )
-    let formatResponse ={
-        _id:request._id,
-        product: request.product,
-        client:clients[0].name,
-        numberPhone: clients[0].numberPhone,
-        status: request.status,
-        price: request.value,
-        destiny: request.destiny,
-        origin: request.origin,
-        destinyUrl: request.destinyUrl,
-        originUrl: request.originUrl,
-        distance: request.distance,
-        reference: request.reference,
-        productValue: request.paymentForRquest,
-        destinationCharge: request.destinationCharge,
-        deliveryId: request.deliveryId
-        }
-    res.send(formatResponse)
-})
-
-router.post('/distance', async (req,res)=>{
-    let request = req.body;
-    let origin = {
-        lat: request.origin.lat,
-        lng: request.origin.lng
+router.get("/client/:id", async (req, res) => {
+  const id = req.params.id
+  let response = []
+  const request = await Request.find()
+  const clients = await User.find()
+  let requestMatch = []
+  request.forEach((e) => {
+    if (e.clientId == id) {
+      requestMatch.push(e)
     }
-    let destiny = {
-        lat: request.destiny.lat,
-        lng: request.destiny.lng
+  })
+  requestMatch.forEach((req) => {
+    let coincidencias = clients.filter((client) => req.userId == client._id)
+    let formatResponse = {
+      _id: req._id,
+      product: req.product,
+      client: coincidencias[0].name,
+      numberPhone: coincidencias[0].numberPhone,
+      status: req.status,
+      price: req.value,
+      address: req.destiny,
     }
-    request.originUrl = request.origin.url; 
-    request.destinyUrl = request.destiny.url; 
-    request.origin = request.origin.name; 
-    request.destiny = request.destiny.name; 
-
-    //aqui
-    let client = await Client.findById({_id:request.clientId});
-    console.log(client)
-    let matrix = await requestMatrixDistane(origin,destiny);
-    let value = await price(matrix, client.type);
-    //aqui
-    const response = {
-        distance: matrix,
-        value: value.cost,
-        paymentForRquest: value.deliverCost
-    }
-    res.send(response);
+    response.push(formatResponse)
+  })
+  res.json(response)
 })
 
-router.post('/', async (req,res) =>{
-    let request = req.body;
-    request.originUrl = request.origin.url; 
-    request.destinyUrl = request.destiny.url; 
-    request.origin = request.origin.name; 
-    request.destiny = request.destiny.name; 
-    let date = new Date()
-    request = {
-        date,
-        status: STATUS[0],
-        ...request
-    }
-    
-    const create = await Request.create(request);
-    const clientId = request.clientId;
-    const billingList = await Billing.find({clientId});
-    let tokens = await Token.find();
-    let listTokens = organiceTokens(tokens);
-    let notificacion = await sendNotification(listTokens);
- 
-    if( billingList.length == 0){
-        const newBilling = {
-            client: request.client,
-            clientId:request.clientId,
-            services: [{
-                product:request.product,
-                value:request.value
-            }],
-            total:request.value,
-            dateExp:new Date(),
-            active:true,
-            number:1,
-            factureDeliver: false
-        }
-        const billingCreate = await Billing.create(newBilling)
-    }else{
-        let listBillings = billingList.filter( billing => billing.active == true );
-        if(listBillings.length > 0){
-            const billingActive = listBillings[0];
-            billingActive.services.push({
-                product:request.product,
-                value:request.value
-            })
-            billingActive.total = billingActive.total + request.value;
-            const billlingUpdate = await Billing.updateOne({_id : billingActive._id}, billingActive) 
-        }else{
-            const newBilling = {
-                client: request.client,
-                clientId:request.clientId,
-                services: [{
-                    product:request.product,
-                    value:request.value
-                }],
-                total:request.value,
-                dateExp:new Date(),
-                active:true,
-                number:billingList[billingList.length - 1].number + 1,
-                factureDeliver: false
-            }
-            const billingCreate = await Billing.create(newBilling)
-        }
-    }
-    res.send(create)
+router.get("/deliver/:id", async (req, res) => {
+  const id = req.params.id
+  const requests = await Request.find({ deliveryId: id })
+  res.send(requests)
 })
 
-router.put('/select/:id', async (req, res)=>{
-    const id = req.params.id;
-    const status = req.body.status.status;
-    const deliver = req.body.status.deliver;
-    const updateRequest = await Request.updateOne({_id:id},{$set:{status: STATUS[status],deliveryId:deliver._id}});
-    const request = await Request.findById({_id:id});
-
-    if( status == 3){
-        const billingList = await Billing.find({clientId:deliver._id});
-        const billingActive = billingList.filter( billing => billing.active == true )[0]
-        if( billingList.length == 0  || !billingActive ){
-            createNewBilling(deliver,request);
-        }else{
-            billingActive.services.push({
-                product:request.product,
-                value:request.paymentForRquest
-            })
-            billingActive.total = billingActive.total + request.paymentForRquest;
-            const billlingUpdate = await Billing.updateOne({_id : billingActive._id}, billingActive) 
-        }
-    }
-
-    res.json(request)
+router.get("/:id", async (req, res) => {
+  const id = req.params.id
+  const request = await Request.findById({ _id: id })
+  const listClients = await User.find()
+  let clients = listClients.filter((client) => request.userId == client._id)
+  let formatResponse = {
+    _id: request._id,
+    product: request.product,
+    client: clients[0].name,
+    numberPhone: clients[0].numberPhone,
+    status: request.status,
+    price: request.value,
+    destiny: request.destiny,
+    origin: request.origin,
+    destinyUrl: request.destinyUrl,
+    originUrl: request.originUrl,
+    distance: request.distance,
+    reference: request.reference,
+    productValue: request.paymentForRquest,
+    destinationCharge: request.destinationCharge,
+    deliveryId: request.deliveryId,
+  }
+  res.send(formatResponse)
 })
 
-router.delete('/all', async(req,res)=>{
-    const response = await Request.deleteMany();
-    res.send(response)
+router.post("/distance", async (req, res) => {
+  let request = req.body
+  let origin = {
+    lat: request.origin.lat,
+    lng: request.origin.lng,
+  }
+  let destiny = {
+    lat: request.destiny.lat,
+    lng: request.destiny.lng,
+  }
+  request.originUrl = request.origin.url
+  request.destinyUrl = request.destiny.url
+  request.origin = request.origin.name
+  request.destiny = request.destiny.name
+
+  //aqui
+  let client = await Client.findById({ _id: request.clientId })
+  let matrix = await requestMatrixDistane(origin, destiny)
+  let value = await price(matrix, client.type)
+  //aqui
+  const response = {
+    distance: matrix,
+    value: value.cost,
+    paymentForRquest: value.deliverCost,
+  }
+  res.send(response)
 })
 
-router.delete('/delete/:id', async (req,res)=>{
-    const id = req.params.id;
-    const response = await Request.findByIdAndDelete({_id: id});
-    res.json(response)
-})
+router.post("/", async (req, res) => {
+  let request = req.body
+  request.originUrl = request.origin.url
+  request.destinyUrl = request.destiny.url
+  request.origin = request.origin.name
+  request.destiny = request.destiny.name
+  let date = new Date()
+  request = {
+    date,
+    status: STATUS[0],
+    ...request,
+  }
 
+  const create = await Request.create(request)
+  const clientId = request.clientId
+  const billingList = await Billing.find({ clientId })
+  let tokens = await Token.find()
+  let listTokens = organiceTokens(tokens)
+  let notificacion = await sendNotification(listTokens)
 
-function organiceTokens(tokens){
-    let list = [];
-    tokens.forEach(token=>{
-        list.push(token.token);
-    })
-    return list;
-}
-
-async function createNewBilling(deliver,request){
-
+  if (billingList.length == 0) {
     const newBilling = {
-        client: deliver.name,
-        clientId:deliver._id,
-        services: [{
-            product:request.product,
-            value:request.paymentForRquest
-        }],
-        total:request.paymentForRquest,
-        dateExp:new Date(),
-        active:true,
-        number:1,
-        factureDeliver: true
+      client: request.client,
+      clientId: request.clientId,
+      services: [
+        {
+          product: request.product,
+          value: request.value,
+        },
+      ],
+      total: request.value,
+      dateExp: new Date(),
+      active: true,
+      number: 1,
+      factureDeliver: false,
     }
     const billingCreate = await Billing.create(newBilling)
+  } else {
+    let listBillings = billingList.filter((billing) => billing.active == true)
+    if (listBillings.length > 0) {
+      const billingActive = listBillings[0]
+      billingActive.services.push({
+        product: request.product,
+        value: request.value,
+      })
+      billingActive.total = billingActive.total + request.value
+      const billlingUpdate = await Billing.updateOne(
+        { _id: billingActive._id },
+        billingActive
+      )
+    } else {
+      const newBilling = {
+        client: request.client,
+        clientId: request.clientId,
+        services: [
+          {
+            product: request.product,
+            value: request.value,
+          },
+        ],
+        total: request.value,
+        dateExp: new Date(),
+        active: true,
+        number: billingList[billingList.length - 1].number + 1,
+        factureDeliver: false,
+      }
+      const billingCreate = await Billing.create(newBilling)
+    }
+  }
+  res.send(create)
+})
 
+router.put("/select/:id", async (req, res) => {
+  const id = req.params.id
+  const status = req.body.status.status
+  const deliver = req.body.status.deliver
+  const updateRequest = await Request.updateOne(
+    { _id: id },
+    { $set: { status: STATUS[status], deliveryId: deliver._id } }
+  )
+  const request = await Request.findById({ _id: id })
+
+  if (status == 3) {
+    const billingList = await Billing.find({ clientId: deliver._id })
+    const billingActive = billingList.filter(
+      (billing) => billing.active == true
+    )[0]
+    if (billingList.length == 0 || !billingActive) {
+      createNewBilling(deliver, request)
+    } else {
+      billingActive.services.push({
+        product: request.product,
+        value: request.paymentForRquest,
+      })
+      billingActive.total = billingActive.total + request.paymentForRquest
+      const billlingUpdate = await Billing.updateOne(
+        { _id: billingActive._id },
+        billingActive
+      )
+    }
+  }
+
+  res.json(request)
+})
+
+router.delete("/all", async (req, res) => {
+  const response = await Request.deleteMany()
+  res.send(response)
+})
+
+router.delete("/delete/:id", async (req, res) => {
+  const id = req.params.id
+  const response = await Request.findByIdAndDelete({ _id: id })
+  res.json(response)
+})
+
+function organiceTokens(tokens) {
+  let list = []
+  tokens.forEach((token) => {
+    list.push(token.token)
+  })
+  return list
 }
 
-module.exports = router;
+async function createNewBilling(deliver, request) {
+  const newBilling = {
+    client: deliver.name,
+    clientId: deliver._id,
+    services: [
+      {
+        product: request.product,
+        value: request.paymentForRquest,
+      },
+    ],
+    total: request.paymentForRquest,
+    dateExp: new Date(),
+    active: true,
+    number: 1,
+    factureDeliver: true,
+  }
+  const billingCreate = await Billing.create(newBilling)
+}
+
+module.exports = router
